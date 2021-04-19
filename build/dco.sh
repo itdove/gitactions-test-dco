e02e0c07408b009438c79b853fcbcc04d8185f17
Dominique Vernier <dvernier@redhat.com>
2021-04-19 09:44:07 -0400

    Change to refs/heads/main
    Signed-off-by: Dominique Vernier <dvernier@redhat.com>

--- build/dco.sh
+++ build/dco.sh
@@ -4,7 +4,7 @@ set -x
 set -e
 BRANCH=$1
 echo "Check all commit signed on branch: ${BRANCH}"
-COMMITS=$(git log main..${BRANCH} -- | grep ^commit | cut -d ' ' -f2)
+COMMITS=$(git log refs/heads/main..${BRANCH} -- | grep ^commit | cut -d ' ' -f2)
 for c in ${COMMITS}
 do
   COMMIT=$(git show ${c} -q)
